74731ecf059d42f7082fa34f6775a812a5595af7
Justin Lee <JustinJ.Lee23@gmail.com>
2016-07-11 10:36:40 -0700

    Initial Commit

---
+++ WKWebViewTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WKWebViewTest
+//
+//  Created by Justin Lee on 7/11/16.
+//  Copyright © 2016 AppLovin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WKWebViewTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WKWebViewTest
+//
+//  Created by Justin Lee on 7/11/16.
+//  Copyright © 2016 AppLovin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
