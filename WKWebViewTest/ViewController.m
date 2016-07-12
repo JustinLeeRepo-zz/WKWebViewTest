efe07bef871b21d19ad84a42804a7b7c6647dc17
Justin Lee <JustinJ.Lee23@gmail.com>
2016-07-11 17:04:14 -0700

    remove bottom half UIWebView. WK is better

--- WKWebViewTest/ViewController.m
+++ WKWebViewTest/ViewController.m
@@ -8,26 +8,30 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <WKNavigationDelegate>
 
 @property (nonatomic, strong) WKWebView *webView;
 @property (nonatomic, strong) UIWebView *oldCompareWebView;
 @property (nonatomic, strong) WKWebViewConfiguration *configuration;
 
+
 @end
 
 @implementation ViewController
+
+#pragma mark - Properties
 
 - (WKWebView *)webView
 {
     if (!_webView) {
         CGRect screen = [[UIScreen mainScreen] bounds];
-        CGRect rect = CGRectMake(0, 0, screen.size.width, screen.size.height / 2);
+        CGRect rect = CGRectMake(0, 0, screen.size.width, screen.size.height);
         _webView = [[WKWebView alloc] initWithFrame:rect configuration:self.configuration];
         
         NSURL *URL = [NSURL URLWithString:@"https://www.applovin.com"];
         NSURLRequest *request = [NSURLRequest requestWithURL:URL];
         [_webView loadRequest:request];
+        
     }
     return _webView;
 }
@@ -54,11 +58,29 @@
     return _oldCompareWebView;
 }
 
+#pragma mark - WKWebView Delegate
+
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler
+{
+    NSLog(@"navigationAction description : %@", [navigationAction description]);
+    decisionHandler(WKNavigationActionPolicyAllow);
+}
+
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationResponse:(WKNavigationResponse *)navigationResponse decisionHandler:(void (^)(WKNavigationResponsePolicy))decisionHandler
+{
+    NSLog(@"!!!! navigationResponse description : %@", [navigationResponse description]);
+    decisionHandler(WKNavigationResponsePolicyAllow);
+}
+
+#pragma mark - View Controller Life Cycle
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    
-    [self.view addSubview:self.oldCompareWebView];
+    self.webView.navigationDelegate = self;
+    self.webView.allowsBackForwardNavigationGestures = YES;
+    self.webView.allowsLinkPreview = YES;
+//    [self.view addSubview:self.oldCompareWebView];
     [self.view addSubview:self.webView];
 }
 
